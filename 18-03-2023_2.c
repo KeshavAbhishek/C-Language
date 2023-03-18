b21937860a4456fa18b1f0366206424876524a29
KESHAV ABHISHEK <crystaled2003@gmail.com>
2023-03-18 21:27:21 +0530

    Upload New

--- 18-03-2023_2.c
+++ 18-03-2023_2.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 int main(){
-    int dataset[2][5]={
+    int dataset[3][5]={
         {1,2,3,4,5},
-        {6,7,8,9,10}
+        {6,7,8,9,10},
+        {11,12,13,14,15}
     };
 
     for (int i = 0; i < 2; i++)
