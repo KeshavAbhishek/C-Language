c4127ee739158301238444ca20d99e4a7abf0d8a
KESHAV ABHISHEK <crystaled2003@gmail.com>
2023-04-26 15:59:57 +0530

    Upload

---
+++ 26-04-2023_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int array[]={1,7,11,20,40,4,90,3};
+    int *ptr;
+    ptr = array;
+    int max = *(ptr);
+    for (int i = 0; i < 8; i++)
+    {
+        if(max < *(ptr+i)){
+            max = *(ptr+i);
+        }
+    }
+    printf("Maximum element in array : %d", max);
+    return 0;
+}
